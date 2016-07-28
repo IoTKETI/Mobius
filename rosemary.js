a060261e265780428c82a79eb617130937a63127
gbsmfather <iyahn@keti.re.kr>
2016-07-29 08:45:11 +0900

    - mobius.js 에서 환경파일 읽어오는 부분 삭제 --> mobius.js 에서 환경 변수들 수정하도록 유도 - usecsetype 이 asn일 때 remoteCSE 생성 요청을 거절하는 부분 추가 - nCube:Rosemary 실행을 위한 rosemary.js 파일 추가 - rosemary.js에서 IN-CSE로 remoteCSE 등록을 위한 이벤트 핸들러 추가

---
+++ rosemary.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2015, OCEAN
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Created by Il Yeup, Ahn in KETI on 2016-07-28.
+ */
+
+var mn = require('./mobius/mn');
+
+global.defaultnmtype = 'short';
+global.defaultbodytype = 'json';
+
+// this environment variable have default value.
+// this value is updated by configuration file.
+
+// parent CSE information
+global.parent_cbname = 'mobius';
+global.parent_cbcseid = '/mobius';
+global.parent_cbhost = 'localhost';
+global.parent_cbhostport = '7579';
+global.parent_cbprotocol = 'http'; // 'http' or 'mqtt'
+
+global.parent_mqttbroker = '203.253.128.151';
+
+// my CSE information
+global.usecsetype = 'mn'; // select 'in or 'mn' or asn'
+global.usecsebase = 'rosemary';
+global.usecseid = '/rosemary';
+global.usecsebaseport = '7575';
+
+global.usepxymqttport = '7574';
+global.usetsagentport = '7573';
+
+global.usedbhost = 'localhost';
+global.usedbpass = 'dksdlfduq2';
+
+global.usemqttbroker = parent_mqttbroker; // mobius to mqttbroker
+global.usemqttcbhost = 'localhost'; // pxymqtt to mobius
+
+require('./app');
+
+global.custom = new process.EventEmitter();
+
+custom.on('register_remoteCSE', function() {
+    mn.build_mn('/'+usecsebase, function (rsp) {
+        console.log('[[[[[[[[[[[[[[[[register_remoteCSE]]]]]]]]]]]]]]]] ' + JSON.stringify(rsp));
+        if(rsp.rsc == '2000') {
+            clearInterval(refreshIntervalId);
+        }
+    });
+});
