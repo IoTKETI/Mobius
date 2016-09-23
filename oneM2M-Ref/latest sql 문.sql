cdcf0b6620308ef2786738cf946639006479bda2
gbsmfather <iyahn@keti.re.kr>
2016-09-23 11:30:03 +0900

    remove a directory

--- oneM2M-Ref/latest sql 문.sql
+++
@@ -1,8 +0,0 @@
-
-select a.*
-from (
-      select ri
-      from lookup
-      where (pi = '/mobius/c067/s')
-      order by ri asc limit 10000
-) b left join lookup as a on b.ri = a.ri where (ty = '4' or ty = '25') limit 1
