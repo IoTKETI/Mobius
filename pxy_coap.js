66782816ddb704cba1350788b30a034a87ee3199
gbsmfather <iyahn@keti.re.kr>
2016-11-02 11:47:28 +0900

    - mobius-yt-2.1.15     -- coap 기능 추가 중

--- pxy_coap.js
+++ pxy_coap.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2015, OCEAN
+ * Copyright (c) 2016, OCEAN
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
@@ -9,8 +9,12 @@
  */
 
 /**
+ * Created by Il Yeup, Ahn in KETI on 2016-10-18.
+ */
+
+/**
  * @file
- * @copyright KETI Korea 2015, OCEAN
+ * @copyright KETI Korea 2016, OCEAN
  * @author Il Yeup Ahn [iyahn@keti.re.kr]
  */
 
