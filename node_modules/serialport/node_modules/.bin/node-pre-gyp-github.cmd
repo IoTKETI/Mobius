93ddf569158b39c65cacd131f5bb037c6fc98508
ryeubi <ryeubi@gmail.com>
2017-07-13 02:56:03 +0900

    - Mobius-2.4.2   -- first commit for Mobius release 2 from local workspace to GitHub of IoTKETI/Mobius

--- node_modules/serialport/node_modules/.bin/node-pre-gyp-github.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\node-pre-gyp-github\bin\node-pre-gyp-github.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\node-pre-gyp-github\bin\node-pre-gyp-github.js" %*
-)
