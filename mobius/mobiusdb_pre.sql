c662c0d5e86b72c0a430fce4a84293f4081539ab
Piero Cena <piero.cena@telecomitalia.it>
2017-10-17 12:03:51 +0200

    preliminary sql statements

--- mobius/mobiusdb_pre.sql
+++ mobius/mobiusdb_pre.sql
@@ -1,14 +1,5 @@
-subscribe resp_topic as /oneM2M/#
-mqtt_message sent: /oneM2M/req/nbiot_cons/onem2m_e65baa96/json 
-{ 'm2m:rqp':
-   { op: '2',
-     to: '/onem2m/nbiot',
-     fr: 'nbiot_cons',
-     rqi: 12345,
-     pc: '' } }
-mqtt_message received:
-        >topic: /oneM2M/req/nbiot_cons/onem2m_e65baa96/json
-        >msg: {"m2m:rqp":{"op":"2","to":"/onem2m/nbiot","fr":"nbiot_cons","rqi":12345,"pc":""}}
-mqtt_message received:
-        >topic: /oneM2M/resp/nbiot_cons/onem2m_e65baa96/json
-        >msg: {"m2m:rsp":{"rsc":2000,"rqi":12345,"pc":{"m2m:ae":{"pi":"Sy2XMSpbb","ty":2,"ct":"20170706T085259","ri":"r1NX_cOiVZ","rn":"nbiot","lt":"20170706T085259","et":"20270706T085259","acpi":["/onem2m/acp_nbiot"],"aei":"nbiot_prod","rr":true}}}}
+CREATE DATABASE mobiusdb;
+
+CREATE USER 'mobius' IDENTIFIED BY 'mobius';
+CREATE USER 'mobius'@'localhost' IDENTIFIED BY 'mobius';
+CREATE USER 'mobius'@'%' IDENTIFIED BY 'mobius';
